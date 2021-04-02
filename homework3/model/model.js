a6e1528ff4bee318ea7210d15b8e2f23cc07f99f
rydeal <rydeal@iu.edu>
2021-04-02 06:55:44 -0400

    fix attempt

---
+++ homework3/model/model.js
@@ -0,0 +1,13 @@
+var MODEL = (function() {
+
+    var _getView = function(viewName){
+        $.get(`views/${viewName}/${viewName}.html`, function(data) {
+            $("#app").html(data);
+        });
+    };
+
+
+    return {
+        getView: _getView,
+    };
+})();
